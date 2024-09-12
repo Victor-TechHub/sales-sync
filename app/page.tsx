580dbeec5eef5c0c5cfc4bb1d9e2eed583d74270
Victor Ezekiel <pianistpro8@gmail.com>
2024-09-12 20:40:30 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      Home
+    <main className="p-5">
+      <Link href="/dashboard">click to view dashboard</Link>
     </main>
   );
 }
